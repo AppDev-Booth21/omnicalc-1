0161f4efb8fbf77bf2153b22fecaa818242c39b3
jweinheibooth <jweinhei@chicagobooth.edu>
2021-07-12 21:26:04 +0000

    Square New working

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ get("/square/new", { :controller => "application", :action => "blank_square_form
 get("/square_root/new", { :controller => "application", :action => ""})
 get("/payment/new", { :controller => "application", :action => ""})
 get("/random/new", { :controller => "application", :action => ""})
-get("/square/results", { :controller => "application", :action => ""})
+get("/square/results", { :controller => "application", :action => "calculate_square"})
 
 
 end
