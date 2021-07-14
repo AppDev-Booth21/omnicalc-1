8657e1b9564ac92af3a8d29d0312e3f94a235363
jweinheibooth <jweinhei@chicagobooth.edu>
2021-07-14 19:06:19 +0000

    Passed the Tests 7.14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,48 @@ class ApplicationController < ActionController::Base
         @square_of_num = @num * @num
         render({ :template => "calculation_templates/square_results.html.erb"})
     end
+   
+    def blank_sqroot_form
+        render({ :template => "calculation_templates/sqroot_form.html.erb"})
+    end
+
+    def calculate_sqroot
+        # params = {"antelope"=>"42"}
+        @num = params.fetch("antelope").to_f
+        @sqroot_of_num = @num ** 0.5
+        render({ :template => "calculation_templates/sqroot_results.html.erb"})
+    end
+
+    def blank_payment_form
+        render({ :template => "calculation_templates/payment_form.html.erb"})
+    end
+
+    def calculate_payment     
+        @monthly = 12
+        @perc = 100
+        @pass=1
+       
+        @apr = params.fetch("rate").to_f
+        @years = params.fetch("time").to_i
+        @pv = params.fetch("cash").to_f
+
+        @apr1 = @apr.to_s( :percentage, { :precision => 4 })
+        @apr2 = @apr / @perc
+        @apr3 = @apr2 / @monthly
+
+       @years2 = @years * @monthly
+
+       @pv2 = @pv.to_s (:currency)
+
+       @tophalf = @apr3 * @pv
+       @bottomhalf1 = @pass-(@pass + @apr3)**(-@years2)
+       # @bottomhalf2 = bottomhalf.to_s( :currency)
+       # @bottomhalf2 = bottomhalf1**(-@years)
+       # @bottomhalf3 = 1-bottomhalf2
+       @payment1 = @tophalf / @bottomhalf1
+       @payment2 = @payment1.to_s (:currency)
+        render({ :template => "calculation_templates/payment_results.html.erb"})
+    end
 
     def blank_random_form
         render({ :template => "calculation_templates/random_form.html.erb"})
